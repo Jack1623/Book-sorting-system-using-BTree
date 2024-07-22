c4ee43e557a4d478faee3ae23b419011180913f6
고채준 <113597653+Jack1623@users.noreply.github.com>
2024-07-22 10:16:25 +0900

    API Settings

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+APIKEY = os.getenv('APIKEY')
+
 class BTreeNode:
     def __init__(self):
         self.values = []
@@ -22,14 +30,14 @@ class BTree:
                 right_node.values = node.values[3:]
                 node.values = [node.values[2]]
                 node.children = [left_node, right_node]
-
-            else:
-                i = 0
-                while i < len(node.values):
-                    if value < node.values[i]:
-                        break
-                    i += 1
-                self.insert_value(node.children[i], value)
+        
+        else:
+            i = 0
+            while i < len(node.values):
+                if value < node.values[i]:
+                    break
+                i += 1
+            self.insert_value(node.children[i], value)  
 
 def print_tree(node, level=0):
     if node is not None:
@@ -43,3 +51,14 @@ for value in values_insert:
     btree.insert(value)
 
 print_tree(btree.root)
+
+baseURL = "https://www.nl.go.kr/NL/search/openApi/search.do?key=" + APIKEY
+
+response = requests.get("https://www.nl.go.kr/NL/search/openApi/search.do?key=" + APIKEY + "&kwd=%ED%86%A0%EC%A7%800&apiType=xml&srchTarget=total") 
+
+if __name__ == '__main__':
+    kwd = input()
+    url = baseURL + "&kwd=" + kwd + "pageSize=10&pageNum=1"
+    response = requests.get(url)
+    url = baseURL
+    print(response)
